f5242f6770f17dc7723ea39beb14cdea57cd4fb2
yurilagos <yuri_lagos@yahoo.com.br>
2016-11-19 19:59:57 -0200

    aula 04

---
+++ Aula04/src/Aula04.java
@@ -0,0 +1,9 @@
+
+public class Aula04 {
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+		c1.status();
+		Caneta c2 = new Caneta("COMPACTO", "Preto", 0.7f);
+		c2.status();
+	}
+}
